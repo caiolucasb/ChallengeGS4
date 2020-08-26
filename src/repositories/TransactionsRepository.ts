f97e9e203ed829f99670f08d759d02c24ab00fd9
caiolucasb <caio.lucas75@gmail.com>
2020-08-26 17:14:44 -0300

    Creating testBalance, setBalance, all and create

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface transactionDTO {
+  title: string;
+  value: number;
+  type: 'income'|'outcome'
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -8,21 +13,47 @@ interface Balance {
 
 class TransactionsRepository {
   private transactions: Transaction[];
+  private balance: Balance;
 
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income:0,
+      outcome:0,
+      total:0
+    }
   }
 
   public all(): Transaction[] {
-    // TODO
+
+    if(false){
+      throw Error("Couldn't get your transactions")
+    }
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public testBalance({type, value}: transactionDTO): true | false {
+
+    if(type == "outcome"){
+      if(this.balance.total < (this.balance.outcome + value))
+        return false
+    }
+    return true
+
+  }
+  public setBalance({type, value}: transactionDTO): Balance {
+
+    type == "income"? this.balance.income += value : this.balance.outcome += value
+    this.balance.total = (this.balance.income-this.balance.outcome)
+
+    return this.balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title,value,type}: transactionDTO): Transaction {
+    const newTransaction = new Transaction({title,value,type})
+    this.transactions.push(newTransaction)
+
+    return newTransaction;
   }
 }
 
